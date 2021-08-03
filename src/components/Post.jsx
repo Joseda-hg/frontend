89634d4a0b8b29565d94914e4e2747f000a6638f
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-08-02 23:35:30 -0400

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,10 +4,10 @@ import { useParams } from "react-router";
 export function Post({ type }) {
   let { id } = useParams();
   return (
-    <>
+    <div>
       Now showing post {id}
       <hr></hr>
       Post type = {type}
-    </>
+    </div>
   );
 }
