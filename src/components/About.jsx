f95a793903d32f1f36fd0df89f018db4ec8da020
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-08-11 13:00:13 -0400

    Updated About and home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,48 @@
 import React from "react";
-import { Editor } from "./Editor"
+// import { Editor } from "./Editor"
 
 export function About() {
   return (
     <div>
-      <h1 id="PageTitle">About Us</h1>
+      <h1 id="PageTitle">Sobre Mi</h1>
       <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et posuere est. Etiam lobortis ornare semper. Nulla nunc est, consectetur at eleifend ac, pulvinar sit amet velit. Integer a ligula id dolor ullamcorper condimentum. Cras nec malesuada massa. Cras ut ex urna. Donec eleifend volutpat malesuada. Phasellus et maximus justo.
+        Una presentacion bastante expedita de mi mismo; Soy un estudiante de
+        tercer semestre de Programacion y Analisis de sistemas, con expectativas
+        de terminar mi formacion como Ingeniero Informatico en la Universidad
+        Andres Bello
+      </p>
+      <p>
+        Soy un entusiasta de las computadoras desde pequeño y he adquirido
+        experiencia con el paso de los años; He sido el encargado de los equipos
+        y sistemas informaticos de la empresa familiar y tengo algunos proyectos
+        de programacion a pequeña escala de los cuales puedes leer mas en la
+        seccion portafolio
+      </p>
+      <p>
+        La mayor parte de mi experiencia se situa en los lenguajes y tecnologias
+        de alto nivel; pero espero hacer el salto a Rust pronto para empezar a
+        trabajar en sistemas con bajas latencias y proyectos mas densos de
+        electronica
+      </p>
+      <p>
+        Mas alla de la computacion que ha sido el foco principal hasta ahora,
+        hablo 2 idiomas; español e Ingles y tengo una proeficiencia basica en
+        Esperanto, en estos momentos estoy interesado en aprender un poco de
+        Aleman Basico. No poseo formacion superior en lingüistica de ningun
+        tipo, aunque no lo descarto de forma pasional
+      </p>
+      <p>
+        Otros proyectos que despiertan mi interes son la escritura, la
+        electronica, la musica y el conocimiento en general, que se dividen el
+        resto del tiempo que tengo; y sobre los cuales pueden leer en mi blog
+      </p>
+      <p>
+        Actualmente funjo como panadero pero tengo experiencia en otros ambitos
+        y espero eventualmente poder dedicarme a la informatica a tiempo
+        completo
+      </p>
 
-Vestibulum tincidunt purus eget gravida accumsan. In sodales suscipit urna ut ullamcorper. Ut enim leo, fringilla nec tincidunt et, pulvinar vitae risus. Praesent urna eros, pretium id lacinia ut, tincidunt ut ligula. Praesent rutrum arcu odio. Nulla eleifend volutpat libero a rutrum. Suspendisse sit amet quam vitae leo vulputate dapibus in at metus. Etiam ac mattis nibh. Cras vel interdum leo. Etiam convallis justo nec augue malesuada, vestibulum commodo dolor auctor. Nulla turpis erat, feugiat id augue et, congue consequat felis. Integer suscipit turpis lectus, eget bibendum nulla rutrum efficitur. Integer congue neque vel diam dapibus hendrerit. Cras varius libero justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-Quisque facilisis vitae justo sit amet gravida. Proin bibendum blandit nisl, sed porttitor massa maximus id. Curabitur nec eros tortor. Vestibulum quis pulvinar enim. Suspendisse rhoncus pretium arcu vel vulputate. Etiam et mauris dolor. Fusce et vestibulum felis. Cras quis mattis risus, a pharetra eros.
-
-Curabitur malesuada augue felis, vitae convallis quam aliquam eu. Vestibulum congue mi nunc, vitae aliquet tellus accumsan quis. Curabitur pellentesque dui id leo interdum pellentesque. Mauris posuere orci velit, quis vestibulum arcu volutpat ac. Nullam volutpat sollicitudin vestibulum. In nibh lacus, facilisis semper rhoncus lacinia, ullamcorper vitae ligula. Duis sollicitudin ornare nulla. Sed pretium arcu ut lobortis pretium. Nulla ultrices neque at lobortis convallis. Duis gravida vel lorem tincidunt malesuada. Donec porttitor at ipsum et luctus.
-
-Morbi ultricies congue augue id aliquam. Integer eu consequat mauris. Vivamus facilisis sem sit amet hendrerit sagittis. Etiam facilisis euismod urna id tincidunt. Quisque commodo at est nec dignissim. Aenean feugiat nisi et tellus vehicula cursus. Quisque eget tellus purus. Morbi non diam ac tellus sodales facilisis ac non nunc. Morbi tempor ornare fermentum.
-
-Duis vestibulum elit libero, ac laoreet diam imperdiet bibendum. Cras mattis venenatis ullamcorper. Etiam varius lorem faucibus nisl suscipit varius. Pellentesque consectetur mi sed urna finibus, in venenatis leo auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent tellus mi, gravida nec pretium et, faucibus id neque. Nulla ac mollis ex. Mauris non neque ac est mattis lobortis a ut nulla. Ut consequat scelerisque vestibulum.
-
-Aenean blandit sed quam eget maximus. Curabitur viverra, nisl sed accumsan consectetur, lacus nulla cursus nisi, eget fringilla justo est vel nulla. Nam et ultrices ipsum. Morbi aliquam magna fringilla ante interdum ultricies. Integer imperdiet posuere quam, sit amet egestas tortor aliquet ac. Nunc in nibh non risus volutpat lacinia. Suspendisse potenti. Suspendisse potenti.
-
-Quisque purus felis, mattis id nunc vel, tincidunt dictum nunc. Nulla fermentum euismod leo imperdiet ultricies. Fusce et feugiat odio. Aenean lorem nibh, hendrerit at justo id, tempus aliquet nulla. Phasellus consectetur erat eget turpis gravida, eget luctus purus consectetur. Morbi at nisi pellentesque est consectetur bibendum consectetur vitae massa. Donec.</p>
-        {/* <Editor /> */}
+      {/* <Editor /> */}
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export function Home() {
   return (
     <>
       <Link to="/ejercicios">
         <button> Llevame a los ejercicios</button>
         {/* <button type="button" class="btn btn-primary btn-lg input-block-level ">Block level button</button> */}
-
       </Link>
+      <p>Soy Jose Hernandez</p>
+      <p>Bienvenido a mi pagina web</p>
+      <p>
+        Soy un desarrollador Junior con conocimientos de Python y Javascript
+      </p>
+      <p>
+        Esta pagina esta construida con React para el frontend y Django para el
+        backend
+      </p>
+      <p>Puedes ver algunos de los proyectos en los que he trabajado aqui</p>
+      <p>Mantengo un blog aqui</p>
+      <p>Algunas de las apps que he publicado estan aqui</p>
+      <p>Y puedes ver mi currriculum aqui</p>
+      <p>Poner un carrusel con las aplicaciones destacas que he construido</p>
+      <p>
+        Hay un formulario de contacto en caso de que quieras dar conmigo o
+        puedes hacerlo a traves de las redes sociales fijadas en el footer de la
+        pagina
+      </p>
     </>
   );
 }
