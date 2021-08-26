4ae6cc7f9fa0ef59411d6e5971836283bf230303
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-08-26 14:38:02 -0400

    modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,41 @@ import { Link } from "react-router-dom";
 export function Home() {
   return (
     <>
+      <button
+        type="button"
+        class="btn btn-danger"
+        data-toggle="modal"
+        data-target=".bd-example-modal-lg"
+      >
+        Lee esto antes de navegar el sitio
+      </button>
+<hr></hr>
+      <div
+        class="modal fade bd-example-modal-lg"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="myLargeModalLabel"
+        aria-hidden="true"
+        id="myModal"
+      >
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            Esta pagina esta aun en desarrollo y de momento se mantiene publica
+            para mero efecto demostrativo, Se espera que contenga contenido
+            incompleto, botones no funcionales, solicitudes que no se envian al
+            servidor, contenido plano sin estilo y opciones solo para usuarios
+            de forma publica, si comprendes todo lo que esto conlleva, eres
+            completamente bienvenido a revisar la pagina a placer, y si asi lo
+            deseas de informar cualquier adicion que recomiendes, o fallo que
+            este fuera de los parametros anteriores
+          </div>
+        </div>
+      </div>
+
       <Link to="/ejercicios">
-        <button type="button" class="btn btn-primary btn-lg input-block-level ">Llevame a los ejercicios</button>
+        <button type="button" class="btn btn-primary btn-lg input-block-level ">
+          Llevame a los ejercicios
+        </button>
       </Link>
       <p>Soy Jose Hernandez</p>
       <p>Bienvenido a mi pagina web</p>
