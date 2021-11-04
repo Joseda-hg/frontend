25faa39bf7e85c0e83e549ee8e229e4df890e9c3
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-11-04 17:17:37 -0300

    Removed unnecesary comment

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,21 +7,7 @@ export function Blog() {
   const { data: BlogList } = useFetch(
     "https://joseda-backend.herokuapp.com/api/ejercicios/"
   );
-
-  // const [BlogList, setBlogList] = useState(null);
-  // useEffect(() => {
-  //   fetch("https://joseda-backend.herokuapp.com/api/blogs/")
-  //   .then(function (response) {
-  //     return response.json();
-  //   })
-  //   .then(function (data) {
-  //     setBlogList(data);
-  //   })
-  //   .catch(function (err) {
-  //     // There was an error
-  //     console.warn("Something went wrong.", err);
-  //   });
-  // }, [])
+  
   return (
     <div>
 {BlogList && <FetchList list={BlogList} />}
