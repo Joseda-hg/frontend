39f648a7461e6ecfeee6db600176b7cf035ac5c8
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-11-14 11:25:40 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,120 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
-require("bootstrap")
+require("bootstrap");
 
 export function Home() {
   useEffect(() => console.log("Use effect ran"), []);
   return (
     <>
-
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-<div id="landingText"><h1 class="atx" id="hola-bienvenid_">Hola, Bienvenid@_</h1>
-<h2 class="atx" id="soy-jose-hernandez-programador-youtuber-y-ocasionalmente-blogger">Soy Jose Hernandez, Programador, Youtuber y ocasionalmente Blogger</h2>
-<p>En esta pagina dedico mi tiempo a crear nuevas aplicaciones para resolver problemas, ocasionalmente escribir algunos <em>insights</em> y exponer mis servicios y productos a quien sea que este interesado.</p>
-<p>Esta pagina fue construida con React y Django, y en ella puedes encontrar algunos de mis proyectos, mi blog y otras creaciones que pueden ser de tu interes</p>
-</div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div id="landingText">
+              <h1 class="atx" id="hola-bienvenid_">
+                Hola, Bienvenid@_
+              </h1>
+              <h2
+                class="atx"
+                id="soy-jose-hernandez-programador-youtuber-y-ocasionalmente-blogger"
+              >
+                Soy Jose Hernandez, Programador, Youtuber y ocasionalmente
+                Blogger
+              </h2>
+              <p>
+                En esta pagina dedico mi tiempo a crear nuevas aplicaciones para
+                resolver problemas, ocasionalmente escribir algunos{" "}
+                <em>insights</em> y exponer mis servicios y productos a quien
+                sea que este interesado.
+              </p>
+              <p>
+                Esta pagina fue construida con React y Django, y en ella puedes
+                encontrar algunos de mis proyectos, mi blog y otras creaciones
+                que pueden ser de tu interes
+              </p>
+            </div>
+          </div>
+          <div class="col-sm">
+            <div className="row">
+              <div
+                id="carouselExampleIndicators"
+                class="carousel slide"
+                data-bs-ride="carousel"
+              >
+                <div class="carousel-indicators">
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="0"
+                    class="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                  ></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img
+                      src="http://placekitten.com/100/300"
+                      class="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="http://placekitten.com/200/300"
+                      class="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="http://placekitten.com/300/300"
+                      class="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <button
+                  class="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                  class="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="next"
+                >
+                  <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    <div class="col-sm">
-      <div className="row">
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" >
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="http://placekitten.com/100/300" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="http://placekitten.com/200/300" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="http://placekitten.com/300/300" class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-      </div>
-
-        
-    </div>
-  </div>
-</div>
-
-
-      
       <button
         type="button"
         class="btn btn-danger"
