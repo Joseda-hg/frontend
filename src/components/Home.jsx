1cd0ef946400b1e65b8971214dd4c62a27b2f232
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-11-27 22:32:36 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -112,7 +112,7 @@ export function Home() {
                 </button>
               </div>
             */}
-            <iframe width="1264" height="480" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="1264" height="480" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
           </div>
         </div>
