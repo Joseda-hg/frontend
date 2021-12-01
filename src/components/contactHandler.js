759bb7bfe19d34d99ed5592caee8608ed0a2f9af
Joseda-hg <71994263+Joseda-hg@users.noreply.github.com>
2021-11-30 22:24:27 -0300

    DF

--- src/components/contactHandler.js
+++ src/components/contactHandler.js
@@ -1,5 +1,5 @@
 export default function  ContactHandler(){
     console.log("Click en Enviar")
-    alert("THANK YOU FOR CONTANTING ME")
+    alert("THANK YOU FOR CONTANTING ME, THIS IS A TEST HANDLER, IT DID NOT SEND ANYTHING FYI")
   }
 
