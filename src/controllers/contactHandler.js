847de464f9830d472b4074d32c744e36d28f97f2
Jose Hernandez <josedhg29@gmail.com>
2022-07-26 21:32:01 -0400

    Removed shit

--- src/controllers/contactHandler.js
+++ src/controllers/contactHandler.js
@@ -1,26 +1,3 @@
-// require('dotenv').config()
-
-// const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey("")
-
-// const msg = {
-//   to: 'mikutorif@gmail.com', // Change to your recipient
-//   from: 'josedhg29@gmail.com', // Change to your verified sender
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-// sgMail
-//   .send(msg)
-//   .then((response) => {
-//     console.log(response[0].statusCode)
-//     console.log(response[0].headers)
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
-
-
 export default function  ContactHandler(e){
     e.preventDefault()
     console.log("Click en Enviar")
