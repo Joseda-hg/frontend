da69f44e39931aa651d8527203ce721a3f1208df
Jose Hernandez <josedhg29@gmail.com>
2022-07-30 00:04:36 -0400

    s

--- src/components/EjercicioList.tsx
+++ src/components/EjercicioList.tsx
@@ -1,23 +1,23 @@
 import React from "react";
+// @ts-ignore
+// function EjercicioItem({/* ejercicio }*/}) {
+//   return (
+//     <div className="card">
+//       {/* <div className="card-header">{ejercicio.nombre}</div>
+//       <div className="card-body">
+//         <h3 className="card-title"> ID del Ejercicio: {ejercicio.id} </h3>
+//         <p className="card-text">
+//           With supporting text below as a natural lead-in to additional content.
+//         </p>
+//         <a href="/" className="btn btn-primary">
+//           Comprobar Ejercicio
+//         </a>
+//       </div> */}
+//     </div>
+//   );
+// }
 
-function EjercicioItem({/* ejercicio }*/}) {
-  return (
-    <div className="card">
-      {/* <div className="card-header">{ejercicio.nombre}</div>
-      <div className="card-body">
-        <h3 className="card-title"> ID del Ejercicio: {ejercicio.id} </h3>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="/" className="btn btn-primary">
-          Comprobar Ejercicio
-        </a>
-      </div> */}
-    </div>
-  );
-}
-
-export function EjercicioList({/* ejercicios }*/}) {
+export function EjercicioList() {
   return (
     <ul id="ejerciciolist">
       {/* {ejercicios.map((ejercicio) => (
