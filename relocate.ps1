3c7efcf5c5cb55220016c58ae72ddc7786c3e042
Jose Hernandez <josedhg29@gmail.com>
2022-07-29 22:34:10 -0400

    gitignore

--- relocate.ps1
+++ relocate.ps1
@@ -3,4 +3,4 @@ npm run build
 Remove-Item -Path C:\DevSpace\Joseda.dev\backend\build -Recurse
 Move-Item -Path C:\DevSpace\Joseda.dev\frontend\build -Destination C:\DevSpace\Joseda.dev\backend\build 
 
-sleep 3000
+Start-Sleep 3000
