015d1d04da7bd5a627ef6c750d9e8168d73e5add
joseda <josedhg29@gmail.com>
2022-08-16 00:44:31 -0400

    a

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+function gameLoop(){
+
+}
+
+function generate(){
+  alert("GENERATED WHATEVER")
+}
+
 export function Clicker() {
   return (
     <>
-<p>Entro en Clicker</p>
+    <p>Entro en Clicker</p>
+    <h1>Whatever Counter</h1>
+    <button onClick={generate}>Generate Whatever</button>
     </>
   );
 }
