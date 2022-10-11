4e074a8d82c169b35bfc46962a86bb6f4252fbe2
joseda <josedhg29@gmail.com>
2022-10-11 17:04:02 -0300

    Removed Unnecesart ContactHandler Call

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ContactHandler from "../controllers/contactHandler";
+// import ContactHandler from "../controllers/contactHandler";
 // import useState from "react"
 
 export function Contact() {
