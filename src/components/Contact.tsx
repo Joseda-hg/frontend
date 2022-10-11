0f43f5ec099a1c1d93116d7982d269f9cf446386
joseda <josedhg29@gmail.com>
2022-10-11 16:37:05 -0300

    Mensaje de contacto en español

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,37 +6,37 @@ export function Contact() {
   return (
     <>
       <div>
-        <h1 id="PageTitle">Contact</h1>
+        <h1 id="PageTitle">Formulario de contacto</h1>
       </div>
       <form action="https://formbold.com/s/3Gjv9" method="POST">
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" name="fromEmail"
+          <label htmlFor="exampleInputEmail1">Direccion de correo electronico</label>
+          <input type="email" name="Sender Email"
             className="form-control"  
             id="direccionMail"
             aria-describedby="emailHelp"
-            placeholder="Enter email" required={true}
+            placeholder="Correo Electronico" required={true}
           ></input>
         </div>
 
-        <label htmlFor="exampleInputEmail1">Name</label>
+        <label htmlFor="exampleInputEmail1">Nombre</label>
         <input
           type="text"
           className="form-control"
-          id="nombre"
+          id="Nombre"
           aria-describedby="emailHelp"
-          placeholder="Enter your name" required={true}
+          placeholder="Nombre" required={true}
           name="nombre"
         ></input>
-        <label htmlFor="exampleInputEmail1">Message</label>
+        <label htmlFor="exampleInputEmail1">Mensaje</label>
         <textarea
           className="form-control"
           id="mensaje"
           aria-describedby="emailHelp"
-          placeholder="Enter your message" required={true}
-          name="Texto"
+          placeholder="Escribe tu mensaje aqui..." required={true}
+          name="Mensaje"
         ></textarea>
-
+{/* 
         <div className="form-check">
           <input
             type="checkbox"
@@ -46,8 +46,9 @@ export function Contact() {
           ></input>
           <label className="form-check-label" htmlFor="exampleCheck1">
             Check me out
-          </label>
+          </label> 
         </div>
+        */}
         {/* Fix Controlled vs Uncontrolled; https://reactjs.org/docs/forms.html#controlled-components, related to the value tag */}
         <button
           type="submit"
